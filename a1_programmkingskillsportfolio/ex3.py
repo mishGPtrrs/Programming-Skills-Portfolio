e73e0020d15d9e7775af439bbd0cb7016efca844
mishGPtrrs <novasgrassblock@gmail.com>
2024-10-26 09:56:41 +0400

    Delete a1_programmkingskillsportfolio directory

--- a1_programmkingskillsportfolio/ex3.py
+++
@@ -1,9 +0,0 @@
-# biography
-
-#get user input
-name = input("enter your name: ")
-hometown = input("enter your hometown: ")
-age = int(input("enter your age: "))
-
-#print user's input
-print("name: ", name,"\nhometown :", hometown, "\nage: ",age)
--- a1_programmkingskillsportfolio/ex4.py
+++
@@ -1,8 +0,0 @@
-#quiz
-
-#
-answer = input("What's the capital of France? ").strip().lower()
-if answer == "paris":
-    print("Correct!")
-else:
-    print("Wrong.")
